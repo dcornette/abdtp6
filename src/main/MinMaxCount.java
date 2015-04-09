4f9c271e2ed6128b734bb6cf495af8f0172f0e4a
Cornette Damien <cornette@a10p21.fil.univ-lille1.fr>
2015-04-09 13:53:04 +0200

    First commit

---
+++ src/main/MinMaxCount.java
@@ -0,0 +1,23 @@
+package main;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class MinMaxCount {
+
+	public static class TokenizerMapper extends
+			Mapper<Object, Text, Text, IntWritable> {
+
+	}
+
+	public static class IntSumReducer extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
